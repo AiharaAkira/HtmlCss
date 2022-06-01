9f44217ec21366fddc232d8a8d042a8d7ed65f68
AiharaAkira <89511903+AiharaAkira@users.noreply.github.com>
2022-06-01 20:19:43 +0900

    faq

--- project/faq/faq.js
+++ project/faq/faq.js
@@ -2,16 +2,16 @@ $(document).ready(function () {
 
 
 
-   /* $("li.faq_question").click(function () {
+   $("li.faq_question").click(function () {
 
         $(this).next().toggle("fast");
 
-    });*/
-
-    $("li.faq_question").mouseover(function () {
-
-        $(this).next().toggle("fast");
     });
+
+   /* $("li.faq_question").mouseover(function () {
+
+        $(this).next().toggle("fast");
+    });*/
 
 
 
